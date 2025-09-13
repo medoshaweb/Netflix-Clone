b01102f7c8fc7231e8d874a725816354dffbdea2
medoshaweb <hailugs@yahoo.com>
2025-09-12 17:18:21 -0700

    change the netflix original to isLargeRow true

--- src/Components/Rows/RowList/RowList.jsx
+++ src/Components/Rows/RowList/RowList.jsx
@@ -5,7 +5,7 @@ import requests from "../../../utils/requests";
 const RowList = () => {
   return (
     <>
-      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRatedMovies} />
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
